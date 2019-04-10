81b7807af194c4085d0aed087e241ec1d1e8bad2
Your Name <a_joviano5@hotmail.com>
2019-04-10 10:28:00 -0300

    aula

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -2,9 +2,9 @@
 
 namespace Hcode\Model;
 
-use Hcode\DB\Sql;
-use Hcode\Model;
-use Hcode\Mailer;
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
 
 class Category extends Model {
 
@@ -88,6 +88,31 @@ class Category extends Model {
         }
     }
 
+    public function getProductsPage($page = 1, $itemPerPage = 8) {
+        $start = ($page - 1) * $itemPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+              SELECT SQL_CALC_FOUND_ROWS * 
+                FROM tb_products a 
+                INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+                WHERE c.idcategory = :idcategory
+            LIMIT $start, $itemPerPage;
+                ", [
+            ':idcategory' => $this->getidcategory()
+        ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data' => Product::checkList($results),
+            'total' => $resultTotal[0]["nrtotal"],
+            'pages' => ceil($resultTotal[0]["nrtotal"] / $itemPerPage)
+        ];
+    }
+
     public function addProduct(Product $product) {
         $sql = new Sql();
         $sql->query("INSERT INTO tb_productscategories(idcategory, idproduct)VALUES(:idcategory, :idproduct)", [
@@ -96,12 +121,12 @@ class Category extends Model {
         ]);
     }
 
-    
-     public function removeProduct(Product $product) {
+    public function removeProduct(Product $product) {
         $sql = new Sql();
         $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [
             ':idcategory' => $this->getidcategory(),
             ':idproduct' => $product->getidproduct()
         ]);
     }
+
 }
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,36 @@ class User extends Model {
     //CONTANTE
     const SECRET = "HcodePhp7_Secret10";
 
+    public static function getFromSession() {
+        $user = new User();
+
+        if (isset($_SESSION[User::SESSION]) && (int) $_SESSION[User::SESSION]['iduser'] . 0) {
+            $user->setData($_SESSION[User::SESSION]);
+        }
+        return $user;
+    }
+
+    public static function checkLogin($inadmin = true) {
+        if (
+                !isset($_SESSION[User::SESSION]) ||
+                !$_SESSION[User::SESSION] ||
+                !(int) $_SESSION[User::SESSION]["iduser"] > 0
+        ) {
+            //NAÕ ESTA LOGADO
+            return false;
+        } else {
+            if ($inadmin === true && $_SESSION[User::SESSION]['inadmin'] === true) {
+
+                return true;
+            } else if ($inadmin === false) {
+
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     public static function login($login, $password) {
         $sql = new Sql();
 
@@ -39,12 +69,7 @@ class User extends Model {
     }
 
     public static function verifyLogin($inadmin = true) {
-        if (
-                !isset($_SESSION[User::SESSION]) ||
-                !$_SESSION[User::SESSION] ||
-                !(int) $_SESSION[User::SESSION]["iduser"] > 0 ||
-                (bool) $_SESSION[User::SESSION]["inadmin"] !== $inadmin
-        ) {
+        if (User::checkLogin($inadmin)) {
 
             header("Location: /admin/login");
             exit;
@@ -112,54 +137,45 @@ class User extends Model {
     }
 
     // public function getForgot($email, $inadmin = true) 
-    public static function getForgot($email, $inadmin = true)
-{
-     $sql = new Sql();
-     $results = $sql->select("
+    public static function getForgot($email, $inadmin = true) {
+        $sql = new Sql();
+        $results = $sql->select("
          SELECT *
          FROM tb_persons a
          INNER JOIN tb_users b USING(idperson)
          WHERE a.desemail = :email;
      ", array(
-         ":email"=>$email
-     ));
-     if (count($results) === 0)
-     {
-         throw new \Exception("Não foi possível recuperar a senha", 1);
- 
-     }
-     else
-     {
-         $data = $results[0];
-         $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
-             ":iduser"=>$data["iduser"],
-             ":desip"=>$_SERVER["REMOTE_ADDR"]
-         ));
-         if (count($results2) === 0)
-         {
-             throw new \Exception("Não foi possível recuperar a senha");
- 
-         }
-         else
-         {
-             $dataRecovery = $results2[0];
-             $dataRecovery = $results2[0];
-             $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-             $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
-             $result = base64_encode($iv.$code);
-             if ($inadmin === true) {
-                 $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
-             } else {
-                 $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$result";
-             } 
-             $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da Hcode Store", "forgot", array(
-                 "name"=>$data["desperson"],
-                 "link"=>$link
-             )); 
-             $mailer->send();
-             return $link;
-         }
-     }
- }
+            ":email" => $email
+        ));
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possível recuperar a senha", 1);
+        } else {
+            $data = $results[0];
+            $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser" => $data["iduser"],
+                ":desip" => $_SERVER["REMOTE_ADDR"]
+            ));
+            if (count($results2) === 0) {
+                throw new \Exception("Não foi possível recuperar a senha");
+            } else {
+                $dataRecovery = $results2[0];
+                $dataRecovery = $results2[0];
+                $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+                $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
+                $result = base64_encode($iv . $code);
+                if ($inadmin === true) {
+                    $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
+                } else {
+                    $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$result";
+                }
+                $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da Hcode Store", "forgot", array(
+                    "name" => $data["desperson"],
+                    "link" => $link
+                ));
+                $mailer->send();
+                return $link;
+            }
+        }
+    }
 
 }
